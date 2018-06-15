e778f48b15b0cb327a25543197e733f939ba5743
seanpatcleary <39105735+seanpatcleary@users.noreply.github.com>
2018-06-15 16:04:07 -0700

    Update iFAMS_Fourier_Transform.py

--- iFAMS_Fourier_Transform.py
+++ iFAMS_Fourier_Transform.py
@@ -7,10 +7,22 @@ Created on Mon Oct  2 15:16:02 2017
 
 import sys
 import numpy as np
+import argparse
 from scipy import interpolate as interpolate
+import matplotlib.pyplot as plt
 import os
 
-file_name = sys.argv[1]
+
+#################### system arguments ##########################
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True, help="name of the file")
+ap.add_argument("-d", "--domain", default = "abs", help="would you like real or absolute values. Default is Absolute,"
+                                                        "type real for real values")
+ap.add_argument("-p", "--plot", default = "no", help="would you like the data plotted?")
+args = vars(ap.parse_args())
+#################### system arguments ##########################
+
+file_name = args["input"]
 f = open(file_name,'r')
 x,y=np.loadtxt (f,
        unpack = True,
@@ -20,7 +32,8 @@ x,y=np.loadtxt (f,
 
 namebase = os.path.splitext(file_name)[0]
 datainterpolation = interpolate.InterpolatedUnivariateSpline(x, y, k=3)
-xnew = np.linspace(np.min(x), np.max(x), np.size(x), endpoint=True) #number of equally-spaced m/z values is equal to number of m/z values in original data
+xnew = np.linspace(np.min(x), np.max(x), np.size(x), endpoint=True) #number of equally-spaced m/z values is equal to
+# number of m/z values in original data
 ynew = datainterpolation(xnew)
 next2power = np.ceil(np.log2(len(ynew)))
 newpadding = pow(2,next2power)-len(ynew)
@@ -34,11 +47,19 @@ np.append(yflip,paddedynew)
 yfull = np.append(yflip,paddedynew)
 maxfreq = np.size(yfull)/expandedspan/2
 ftx = np.linspace(0, maxfreq, np.size(yfull), endpoint=True)
-FT = np.fft.fft(yfull)      
-ABFT=np.absolute(FT)
+FT = np.fft.fft(yfull)
+if args["domain"] == "real":
+    ABFT=np.real(FT)
+    print ("working")
+else:
+    ABFT=np.absolute(FT)
 
 ftstring = "FT"
 ftfilename = namebase + ftstring + ".csv"
-ftarrayforexport = np.transpose([ftx,ABFT])        
+ftarrayforexport = np.transpose([ftx,ABFT])
+
 np.savetxt(ftfilename,ftarrayforexport,fmt='%10.6f',delimiter=',')#outputs the FFT data to its own file
-print ("File was Exported")
+print ("your file was Fourier transformed and the data was exported")
+if args["plot"] == "yes":
+    plt.plot(ftx,ABFT)
+    plt.show()
